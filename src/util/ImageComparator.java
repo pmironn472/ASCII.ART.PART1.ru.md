3e55d9af5e311a7fcae1807fe21fe07a3b6132e6
pmironn472 <63416798+pmironn472@users.noreply.github.com>
2020-06-28 18:11:34 +0300

    Add files via upload

---
+++ src/util/ImageComparator.java
@@ -0,0 +1,30 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ImageComparator {
+    int count = 0;
+
+    public int compareTwo(String sourceFile, String modifiedFile) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader(sourceFile));
+        BufferedReader br1 = new BufferedReader(new FileReader(modifiedFile));
+        int c, b;
+        while ((c = br.read()) != -1) {
+            b = br1.read();
+            if (c == b) return 0;
+            else count++;
+        }
+        return count;
+    }
+
+    public boolean compare(String sourceFile, String modifiedFile) throws IOException {
+        ImageComparator ic = new ImageComparator();
+        if (ic.compareTwo(sourceFile,modifiedFile) == 0) {
+            return true;
+        } else
+            return false;
+    }
+}
